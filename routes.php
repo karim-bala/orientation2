8ce05cbe50543cf15adc687575a78983857676e8
Abdelkarim Bala <66967272+karim-bala@users.noreply.github.com>
2024-06-30 10:52:31 +0100

    Add files via upload

---
+++ routes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/note' => 'controllers/notes/show.php',
+    '/notes/create' => 'controllers/notes/create.php',
+    '/contact' => 'controllers/contact.php',
+    '/login' => 'controllers/etudiant/login.php',
+    '/admin/login'=>'controllers/admin/login.php',
+    '/admin/main' =>'controllers/admin/main.php',
+    '/etudiant' => 'controllers/etudiant/main.php'
+];
